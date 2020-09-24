f977ae5cd741a908beda59ba568fcaa443d42f44
Anton <sqandex1suggestions@gmail.com>
2020-09-24 20:37:21 +0200

    Initial commit

---
+++ src/main/java/com/dreadblade/lightair/LightAirApplication.java
@@ -0,0 +1,13 @@
+package com.dreadblade.lightair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightAirApplication.class, args);
+	}
+
+}
