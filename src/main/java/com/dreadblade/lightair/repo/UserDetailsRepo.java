8e616a3e2b72774aa5963ea9202be508b6d71759
Anton <sqandex1suggestions@gmail.com>
2020-09-25 18:53:33 +0200

    Added an oauth2 client

---
+++ src/main/java/com/dreadblade/lightair/repo/UserDetailsRepo.java
@@ -0,0 +1,7 @@
+package com.dreadblade.lightair.repo;
+
+import com.dreadblade.lightair.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDetailsRepo extends JpaRepository<User, String> {
+}
